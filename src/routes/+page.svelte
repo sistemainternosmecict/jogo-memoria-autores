b2c546d9aac30c617d9d3679df46e586f598a7fd
thyezoliveira <thyezoliveira@gmail.com>
2025-10-29 16:11:30 -0300

    fix(ui): Responsividade do header do site.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -87,12 +87,12 @@
 
 <div class="select-none content flex min-h-screen flex-col bg-[#ffffff] text-white">
 	<section class="flex flex-wrap justify-center">
-		<div class="shadow-xl flex items-center bg-[#063636] p-4 z-1 justify-between  w-screen ">
+		<div class="shadow-xl flex flex-col sm:flex-row items-center bg-[#063636] p-4 z-1 justify-between  w-screen ">
 			<h1 class="text-shadow-lg text-center text-3xl font-bold poppins-bold text-[#ffffff]">Jogo da Memória <span class="text-[#5291ad] poppins-thin">Autores</span></h1>
 			
-			<section class="p-2 flex items-center">
-				<img class="drag-none m-2 object-contain max-h-[80px] drop-shadow-[0_1px_2px_rgba(0,0,0,1)]" src="/LOGO_FLIS.svg" alt="logo da flis" />
-				<img class="drag-none m-2 object-contain max-h-[60px] drop-shadow-[0_1px_2px_rgba(0,0,0,1)]" src="/sub_logo.svg" alt="logo da sub de tecnologia" />
+			<section class="p-2 flex flex-row items-center">
+				<img class="drag-none m-2 max-w-20 sm:max-w-28 min-w-[0] drop-shadow-[0_1px_2px_rgba(0,0,0,1)]" src="/LOGO_FLIS.svg" alt="logo da flis" />
+				<img class="drag-none m-2 max-w-34 sm:max-w-54 min-w-[0] drop-shadow-[0_1px_2px_rgba(0,0,0,1)]" src="/sub_logo.svg" alt="logo da sub de tecnologia" />
 			</section>
 		</div>
 	</section>
