87a226b35c139e00f908fe84531df7c75d3c47be
Romeiros-ux <marceloromeirofilho@smec.saquarema.rj.gov.br>
2025-11-04 11:47:58 -0300

    Corrige configuração para Netlify - remove dependência de fs

--- src/routes/memoria/+page.server.ts
+++ src/routes/memoria/+page.server.ts
@@ -1,18 +1,23 @@
-import { resolve } from 'path';
 import type { PageServerLoad } from './$types';
-import fs from 'fs';
+
+export const prerender = true;
 
 export const load: PageServerLoad = ({ params }) => {
-	const subDir = 'cartas';
-	const root = './static/';
-	const directory = resolve(`${root}${subDir}`);
-	const files = fs.readdirSync(directory);
+	// Lista hardcoded das cartas disponíveis
+	const cartas = [
+		'cecilia_1', 'cecilia_2', 'cecilia_3', 'cecilia_4',
+		'clarice_1', 'clarice_2', 'clarice_3', 'clarice_4',
+		'conceicao_1', 'conceicao_2', 'conceicao_3', 'conceicao_4',
+		'cora_1', 'cora_2', 'cora_3', 'cora_4',
+		'rachel_1', 'rachel_2', 'rachel_3', 'rachel_4'
+	];
+
 	return {
-		options: files.map((fileName) => ({
-			name: fileName.split('.')[0],
-			path: `${subDir}/${fileName}`,
+		options: cartas.map((fileName) => ({
+			name: fileName,
+			path: `cartas/${fileName}.png`,
 			carta: fileName.split('_')[0],
-			variante: fileName.split('.')[0].split('_')[1]
+			variante: fileName.split('_')[1]
 		}))
 	};
 };
